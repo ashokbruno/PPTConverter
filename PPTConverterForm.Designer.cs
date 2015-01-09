ce4ecddb7a1f645f6e9998aead1b4378da02cf2a
ashokbruno <ashokbruno@gmail.com>
2015-01-09 09:25:08 -0500

    first rev

---
+++ PPTConverterForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PPTConverter
+{
+	partial class PPTConverterForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBoxFiles = new System.Windows.Forms.ListBox();
+			this.textBoxFolder = new System.Windows.Forms.TextBox();
+			this.FolderDlgBtn = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.btnConvertSelected = new System.Windows.Forms.Button();
+			this.btnConvertAll = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// listBoxFiles
+			// 
+			this.listBoxFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.listBoxFiles.FormattingEnabled = true;
+			this.listBoxFiles.ItemHeight = 16;
+			this.listBoxFiles.Location = new System.Drawing.Point(22, 92);
+			this.listBoxFiles.Name = "listBoxFiles";
+			this.listBoxFiles.Size = new System.Drawing.Size(313, 244);
+			this.listBoxFiles.TabIndex = 2;
+			// 
+			// textBoxFolder
+			// 
+			this.textBoxFolder.AllowDrop = true;
+			this.textBoxFolder.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+			this.textBoxFolder.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystemDirectories;
+			this.textBoxFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxFolder.Location = new System.Drawing.Point(22, 30);
+			this.textBoxFolder.Name = "textBoxFolder";
+			this.textBoxFolder.Size = new System.Drawing.Size(411, 22);
+			this.textBoxFolder.TabIndex = 0;
+			this.textBoxFolder.TextChanged += new System.EventHandler(this.OnFolderTextChanged);
+			this.textBoxFolder.DragDrop += new System.Windows.Forms.DragEventHandler(this.OnDragDrop);
+			this.textBoxFolder.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnFolderDragEnter);
+			// 
+			// FolderDlgBtn
+			// 
+			this.FolderDlgBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FolderDlgBtn.Location = new System.Drawing.Point(457, 30);
+			this.FolderDlgBtn.Name = "FolderDlgBtn";
+			this.FolderDlgBtn.Size = new System.Drawing.Size(76, 23);
+			this.FolderDlgBtn.TabIndex = 1;
+			this.FolderDlgBtn.Text = "Browse...";
+			this.FolderDlgBtn.UseVisualStyleBackColor = true;
+			this.FolderDlgBtn.Click += new System.EventHandler(this.FolderDlgBtn_Click);
+			// 
+			// btnConvertSelected
+			// 
+			this.btnConvertSelected.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnConvertSelected.Location = new System.Drawing.Point(396, 92);
+			this.btnConvertSelected.Name = "btnConvertSelected";
+			this.btnConvertSelected.Size = new System.Drawing.Size(137, 47);
+			this.btnConvertSelected.TabIndex = 3;
+			this.btnConvertSelected.Text = "Convert Selected";
+			this.btnConvertSelected.UseVisualStyleBackColor = true;
+			this.btnConvertSelected.Click += new System.EventHandler(this.btnConvertSelected_Click);
+			// 
+			// btnConvertAll
+			// 
+			this.btnConvertAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnConvertAll.Location = new System.Drawing.Point(396, 167);
+			this.btnConvertAll.Name = "btnConvertAll";
+			this.btnConvertAll.Size = new System.Drawing.Size(137, 47);
+			this.btnConvertAll.TabIndex = 4;
+			this.btnConvertAll.Text = "Convert All";
+			this.btnConvertAll.UseVisualStyleBackColor = true;
+			this.btnConvertAll.Click += new System.EventHandler(this.btnConvertAll_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(109, 353);
+			this.progressBar1.Maximum = 10000;
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(378, 23);
+			this.progressBar1.Step = 100;
+			this.progressBar1.TabIndex = 5;
+			// 
+			// PPTConverterForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(573, 398);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.btnConvertAll);
+			this.Controls.Add(this.btnConvertSelected);
+			this.Controls.Add(this.FolderDlgBtn);
+			this.Controls.Add(this.textBoxFolder);
+			this.Controls.Add(this.listBoxFiles);
+			this.Name = "PPTConverterForm";
+			this.Text = "Powerpoint Converter";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PPTConverterForm_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBoxFiles;
+		private System.Windows.Forms.TextBox textBoxFolder;
+		private System.Windows.Forms.Button FolderDlgBtn;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Button btnConvertSelected;
+		private System.Windows.Forms.Button btnConvertAll;
+		private System.Windows.Forms.ProgressBar progressBar1;
+	}
+}
+
---
+++ PPTConverterForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Core;
+
+namespace PPTConverter
+{
+	public partial class PPTConverterForm : Form
+	{
+		public PPTConverterForm()
+		{
+			InitializeComponent();
+
+			if (Application.UserAppDataRegistry.GetValue("FolderLocation") != null)
+			{
+				bool bLoad = false;
+				string folderLocation = "";
+				try
+				{
+					folderLocation = Application.UserAppDataRegistry.GetValue("FolderLocation").ToString();
+					System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(folderLocation);
+
+					if (dir.Exists)
+						bLoad = true;
+				}
+				catch (Exception)
+				{
+				}
+
+				if (bLoad)
+					textBoxFolder.Text = folderLocation;
+				else
+				{
+					System.IO.DirectoryInfo currentDirectory = new System.IO.DirectoryInfo("h:");
+					if (currentDirectory.Exists)
+						textBoxFolder.Text = folderLocation;
+
+				}
+				UpdateFilesList();
+			}
+		}
+
+		public void UpdateFilesList()
+		{
+			bool bLoad = false;
+			DirectoryInfo dir = null;
+			try
+			{
+				dir = new DirectoryInfo(textBoxFolder.Text);
+
+				if (dir.Exists)
+					bLoad = true;
+			}
+			catch (Exception)
+			{
+			}
+
+			if (!bLoad)
+				return;
+
+			listBoxFiles.Items.Clear();
+			if (textBoxFolder.Text.Length > 0)
+			{
+				listBoxFiles.BeginUpdate();
+
+				if (dir.Exists)
+				{
+					foreach (FileInfo f in dir.GetFiles("*.ppt"))
+					{
+						listBoxFiles.Items.Add(f.Name.Replace(".ppt", ""));
+					}
+				}
+				listBoxFiles.EndUpdate();
+			}
+		}
+
+		private void OnFolderDragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effect = DragDropEffects.Copy;
+			}
+			else
+			{
+				e.Effect = DragDropEffects.None;
+			}
+		}
+
+		private void OnDragDrop(object sender, DragEventArgs e)
+		{
+			string fileName = "";
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				string[] fileNames;
+				fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+				fileName = fileNames[0];
+			}
+			bool bLoad = false;
+			try
+			{
+				System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(fileName);
+
+				if (dir.Exists)
+					bLoad = true;
+			}
+			catch (Exception)
+			{
+			}
+
+			if (bLoad)
+			{
+				textBoxFolder.Text = fileName;
+				UpdateFilesList();
+			}
+		}
+
+		private void OnFolderTextChanged(object sender, EventArgs e)
+		{
+			UpdateFilesList();
+		}
+
+		private void FolderDlgBtn_Click(object sender, EventArgs e)
+		{
+			DialogResult dialogResult = folderBrowserDialog1.ShowDialog();
+			if (dialogResult.Equals(DialogResult.OK))
+			{
+				textBoxFolder.Text = folderBrowserDialog1.SelectedPath;
+
+				UpdateFilesList();
+			}
+		}
+
+		private void PPTConverterForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Application.UserAppDataRegistry.SetValue("FolderLocation", textBoxFolder.Text);
+
+			Application.Exit();
+		}
+
+		private void btnConvertSelected_Click(object sender, EventArgs e)
+		{
+			string strFullFilePath = "";
+
+			int iSelectedIndex = listBoxFiles.SelectedIndex;
+			if (iSelectedIndex >= 0)
+			{
+
+				string strLogName = listBoxFiles.SelectedItem.ToString();
+				strFullFilePath = strLogName;
+			}
+			ConvertFile(textBoxFolder.Text + "\\" + strFullFilePath);
+		}
+
+		private void btnConvertAll_Click(object sender, EventArgs e)
+		{
+			progressBar1.Visible = true;
+			progressBar1.Maximum = listBoxFiles.Items.Count;
+			progressBar1.Step = 1;
+			Microsoft.Office.Interop.PowerPoint.Application pptApp = new Microsoft.Office.Interop.PowerPoint.Application();
+			foreach (object logListItem in listBoxFiles.Items)
+			{
+				progressBar1.PerformStep();
+				this.Refresh();
+
+				string strLogName = logListItem.ToString();
+				string strFullFilePath = textBoxFolder.Text + "\\" + strLogName;
+
+				if (File.Exists(strFullFilePath + ".ppt"))
+				{
+					Microsoft.Office.Interop.PowerPoint.Presentation pptPresentation = pptApp.Presentations.Open(strFullFilePath + ".ppt", MsoTriState.msoFalse, MsoTriState.msoFalse, MsoTriState.msoFalse);
+
+					pptPresentation.SaveAs(strFullFilePath + ".pptx", Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType.ppSaveAsOpenXMLPicturePresentation);
+					pptPresentation.Close();
+				}
+			}
+			pptApp.Quit();
+			progressBar1.Visible = false;
+		}
+
+		private void ConvertFile(string strFilePath)
+		{
+
+			if (File.Exists(strFilePath + ".ppt"))
+			{
+				Microsoft.Office.Interop.PowerPoint.Application pptApp = new Microsoft.Office.Interop.PowerPoint.Application();
+				Microsoft.Office.Interop.PowerPoint.Presentation pptPresentation = pptApp.Presentations.Open(strFilePath + ".ppt", MsoTriState.msoFalse, MsoTriState.msoFalse, MsoTriState.msoFalse);
+
+				pptPresentation.SaveAs(strFilePath + ".pptx", Microsoft.Office.Interop.PowerPoint.PpSaveAsFileType.ppSaveAsOpenXMLPicturePresentation);
+				pptPresentation.Close();
+
+				pptApp.Quit();
+			}
+		}
+	}
+}
